ddc7c5a2d6204ba14cce267469442574bd9868de
Jared Ning <jared@redningja.com>
2017-06-21 11:18:48 -0500

    Build Farm.

--- concept.rb
+++ concept.rb
@@ -1,22 +1,80 @@
+class Farm
+  attr_reader :racks
+  attr_reader :rows, :cols
+
+  def initialize(rows, cols)
+    @rows = rows
+    @cols = cols
+    @racks = {}
+  end
+
+  def add_rack(rack, row, col)
+    rack.row = row
+    rack.col = col
+    @racks[rack.location] = rack
+    rack
+  end
+end
+
 class Rack
+  attr_accessor :farm
+  attr_accessor :row, :col
   attr_reader :servers
+  SHELVES = 2
 
   def initialize
-    @servers = []
+    @servers = Array(SHELVES)
+  end
+
+  def location
+    @location ||= "#{row}:#{col}"
+  end
+
+  def add_server(server, idx)
+    @servers[idx] = server
+    server.rack = self
+    server
   end
 end
 
 class Server
   attr_accessor :rack
   attr_reader :cards
-  attr_accessor :location
-  alias_method :name, :location
+
+  SLOTS = 11
 
   def initialize
-    @cards = []
+    @cards = Array(SLOTS)
+  end
+
+  def add_card(card, slot)
+    @cards[slot] = card
+    card.server = self
+    card
   end
 end
 
 class Card
   attr_accessor :server
+end
+
+farm = Farm.new(5, 4)
+
+farm.rows.times do |row|
+  farm.cols.times do |col|
+
+    rack = Rack.new
+    farm.add_rack(rack, row, col)
+
+    Rack::SHELVES.times do |shelf|
+      server = Server.new
+      rack.add_server server, shelf
+
+      Server::SLOTS.times do |slot|
+        card = Card.new
+        server.add_card(card, slot)
+      end
+    end
+
+  end
 end
